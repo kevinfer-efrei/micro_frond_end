b85bdf7b688e4a422151507fa8a6b67dec4a89b1
Paul DUPLESSI <paul.duplessi@hotmail.fr>
2024-02-09 17:00:42 +0100

    add vitest tests

--- cra/vitest/vitest.config.ts
+++ cra/vitest/vitest.config.ts
@@ -1,18 +1,19 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vitest/config';
-import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
- 
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vitest/config';
+
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
+    setupFiles: [__dirname + '/vitest-setup.js'],
   },
   resolve: {
     alias: {
-      NOM_DOSSIER_SHARED_COMPONENT: resolve(__dirname, '../NOM_DOSSIER_SHARED_COMPONENT/DOSSIER_COMPONENT'),
+      remote: resolve(__dirname, '../remote/src'),
+      host: resolve(__dirname, '../host/src'),
     },
   },
 });
- 
