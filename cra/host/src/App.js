2247d71a2f3c59e7c7c978209f2009306cb35108
Paul DUPLESSI <paul.duplessi@hotmail.fr>
2024-02-09 14:14:47 +0100

    add shared header and footer in host app

--- cra/host/src/App.js
+++ cra/host/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import SharedHeader from '../../sharedcomponent/shared/Header'
+import SharedFooter from '../../sharedcomponent/shared/Footer'
 
 const RemoteButton = React.lazy(() => import('remote/Button'));
 
 const App = () => (
   <div>
+    <SharedHeader />
     <h1>Basic Host-Remote</h1>
     <h2>Host</h2>
     <React.Suspense fallback="Loading Button">
       <RemoteButton />
     </React.Suspense>
+    <SharedFooter />
   </div>
 );
 
