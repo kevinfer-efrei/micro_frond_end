0e2824c72f41571be1ad31c6282f3edcf1c28d21
Paul DUPLESSI <paul.duplessi@hotmail.fr>
2024-02-09 17:11:40 +0100

    add footer style

--- cra/sharedcomponent/shared/Footer.jsx
+++ cra/sharedcomponent/shared/Footer.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 
 function Footer() {
+    const footerStyle = {
+        backgroundColor: '#f0f0f0',
+        color: '#333',
+        textAlign: 'center',
+        padding: '10px',
+        position: 'fixed',
+        left: '0',
+        bottom: '0',
+        width: '100%',
+    };
+
     return (
-        <footer>
+        <footer style={footerStyle}>
             <h6>ⓒ Paul & Kevin Consulting 2024 tous droits réservés</h6>
         </footer>
     );
